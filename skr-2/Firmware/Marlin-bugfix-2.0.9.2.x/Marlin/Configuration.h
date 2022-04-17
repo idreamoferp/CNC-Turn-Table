29153c5628ddc572fe007a85054abb36802d212a
Justin Mangini <justin@idreamoferp.com>
2022-04-17 06:30:20 -0600

    changeauthor and machine name

--- skr-2/Firmware/Marlin-bugfix-2.0.9.2.x/Marlin/Configuration.h
+++ skr-2/Firmware/Marlin-bugfix-2.0.9.2.x/Marlin/Configuration.h
@@ -69,7 +69,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(BigTreeTech, SKR-2)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(iDreamOfERP, Justin Mangini)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -143,7 +143,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-//#define CUSTOM_MACHINE_NAME "3D Printer"
+#define CUSTOM_MACHINE_NAME "CNC Turn Table"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
